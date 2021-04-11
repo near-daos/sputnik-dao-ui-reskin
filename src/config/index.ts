8e669cf889ece7c564151baeb027cfc0ecf91faa
Denis Krasovskyi <denis.krasovskyi@gmail.com>
2021-04-11 18:07:01 +0000

    Merged in feature/integration (pull request #49)
    Feature/integration
    
    * UI fixes
    
    * Proposal creation integrated
    
    * Merge branch 'master' of bitbucket.org:magicpowered/sputnik-dao into feature/integration
    
    * Changed network to testnet

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,4 @@ import { getNearConfig } from './near';
 
 export type { NearConfig } from './near';
 
-export const nearConfig = getNearConfig(process.env.NODE_ENV);
+export const nearConfig = getNearConfig('development'); // only for testing
